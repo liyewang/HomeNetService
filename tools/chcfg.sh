4b0312988025768e4be4011bf0d4e1743a6108de
Liye Wang <liyewang0602@qq.com>
2021-12-16 00:09:08 +0800

    Update.

--- tools/chcfg.sh
+++ tools/chcfg.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#chown root /root/config.json
-#chgrp root /root/config.json
-#mv /root/config.json /usr/local/etc/v2ray/config.json
 v2ray -test -config /usr/local/etc/v2ray/config.json
 echo v2ray restart...
 systemctl restart v2ray
--- tools/cpu_temp.sh
+++ tools/cpu_temp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while true
 do
-echo `date '+%Y/%m/%d %H:%M:%S'`: `/opt/vc/bin/vcgencmd measure_temp`
-# cat /sys/class/thermal/thermal_zone0/temp
+# echo `date '+%Y/%m/%d %H:%M:%S'`: `/opt/vc/bin/vcgencmd measure_temp`
+temp=`cat /sys/class/thermal/thermal_zone0/temp`
+echo `date '+%Y/%m/%d %H:%M:%S'`: `expr $temp / 1000`.`expr $temp % 1000`
 sleep 0.5
 done
