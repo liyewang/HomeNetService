7fe73db998f3cdeae5bc9653f085d7aaa26a1d6b
Liye Wang <liyewang0602@qq.com>
2022-03-06 21:29:23 +0800

    Add energy meter.

---
+++ energy_meter.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import requests
+import random
+import re
+import time
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+# Energy Meter Config
+meter_user = 'user'
+meter_pass = 'pass'
+proxy = {}
+# proxy = {'http': 'http://101.231.121.17:80'}
+energy_prev = 61.16
+cost_prev = 43.48
+balance_low = -80
+#              Jan    Feb     Mar    Apr    May    Jun    Jul    Aug    Sep    Oct    Nov    Dec
+price_tab = [0.805, 0.711, 0.8251, 0.711, 0.711, 0.711, 0.738, 0.738, 0.738, 0.711, 0.711, 0.711]
+data_intv = 30
+clock_offset = 1
+power_max = 15
+energy_err_max = 0.01
+
+# File Path
+energy_meter_path = os.path.dirname(os.path.abspath(__file__))
+data_path = f'{energy_meter_path}/energy_meter'
+log_path = f'{energy_meter_path}/energy_meter.log'
+
+# Mail Server
+mail_host = 'smtp.qq.com'
+mail_port = 465
+mail_user = "user@qq.com"
+mail_pass = "pass"
+# Mail Address
+mail_from_addr = 'user@qq.com'
+mail_to_addrs = ['dest@qq.com']
+# Mail Content
+mail_from = 'Energy Meter Server <noreply@energymeter.com>'
+mail_subject = 'Energy Meter Low Balance'
+mail_text = 'Error message.'
+
+def log(msg):
+    print(msg)
+    t = time.strftime(r'%Y/%m/%d %H:%M:%S', time.localtime())
+    f = open(log_path, 'a')
+    f.write(f'{t}: {msg}\n')
+    f.close()
+    return
+
+def login(meter_user, meter_pass):
+    url_login = 'http://tzjz.acrel-eem.com/Ajax/UserLogin.ashx?Id=' + str(random.random()) + '&username=' + meter_user + '&password=' + meter_pass
+    r = requests.get(url=url_login, proxies=proxy)
+    return requests.utils.dict_from_cookiejar(r.cookies)
+
+def get_balance(cookie):
+    r = requests.get(url='http://tzjz.acrel-eem.com/Ajax/CheckUserLogin.ashx?Id=2', cookies=cookie, proxies=proxy)
+    m = re.search(r'id="(.+?)"', r.text)
+    cookie['InterID'] = requests.utils.quote(m.group(1))
+    m = re.search(r'(-?\d+\.\d+)元', r.text)
+    return float(m.group(1))
+
+def get_energy(cookie):
+    if not cookie['InterID']:
+        get_balance(cookie)
+    StartDate = time.strftime(r'%Y-%m-%d', time.localtime(time.time() - 24 * 3600))
+    EndDate = time.strftime(r'%Y-%m-%d', time.localtime(time.time() + 24 * 3600))
+    url_data = 'http://tzjz.acrel-eem.com/Ajax/CheckUserLogin.ashx?Id=7&StartDate=' + StartDate + '&EndDate=' + EndDate
+    r = requests.get(url=url_data, cookies=cookie, proxies=proxy)
+    m = re.search(r'(\d+\.\d+)度</p>', r.text)
+    return float(m.group(1))
+
+def get_topup(cookie):
+    r = requests.get(url='http://tzjz.acrel-eem.com/Ajax/CheckUserLogin.ashx?Id=6', cookies=cookie, proxies=proxy)
+    m = re.findall(r'(\d+\.\d+)元', r.text)
+    topup = 0
+    for t in m:
+        topup += float(t)
+    return topup
+
+def metering(balance, topup, energy_real):
+    global tm, cost, cost_prev, energy, energy_prev, price_tab
+    price = price_tab[time.localtime().tm_mon - 1]
+    cost_delta = topup - balance - cost
+    cost = topup - balance
+    energy_delta = cost_delta / price
+    energy += energy_delta
+    # Calib energy and price
+    if (time.localtime().tm_min - clock_offset) == 0:
+        if abs(energy_real - energy) > energy_err_max:
+            price = (cost - cost_prev) / (energy_real - energy_prev)
+            energy_delta = cost_delta / price
+            energy = energy_real
+        energy_prev = energy_real
+        cost_prev = cost
+    elif abs(energy_real - energy) > power_max * data_intv / 60:
+        energy = energy_real
+    # Calculate power
+    t_delta = time.mktime(time.localtime()) - time.mktime(tm)
+    if t_delta >= data_intv * 60 * 2:
+        power = energy_delta * 3600 / t_delta
+    else:
+        power = energy_delta * 60 / data_intv
+    if power > power_max:
+        power = 0
+    return price, power
+
+def balance_notify(balance):
+    global mail_balance
+    if balance <= mail_balance:
+        mail_text = f'Low balance: {balance}'
+        try:
+            smtpObject = smtplib.SMTP_SSL(mail_host, mail_port)
+            smtpObject.login(mail_user, mail_pass)
+            for mail_to_addr in mail_to_addrs:
+                message = MIMEText(mail_text, 'plain', 'utf-8')
+                message['From'] = Header(mail_from, 'utf-8')
+                message['To'] =  Header('<' + mail_to_addr + '>', 'utf-8')
+                message['Subject'] = Header(mail_subject, 'utf-8')
+                smtpObject.sendmail(mail_from_addr, [mail_to_addr], message.as_string())
+                log(f'Mail to [{mail_to_addr}] sent.')
+        except:
+            log('Sending mail failed.')
+        else:
+            log('Sending mail success.')
+            mail_balance -= 10
+        finally:
+            smtpObject.quit()
+    elif balance > balance_low:
+        mail_balance = balance_low
+    return
+
+# Init
+tm = time.localtime()
+energy = energy_prev
+cost = cost_prev
+mail_balance = balance_low
+cookie = {}
+# Get previous data
+f = open(data_path, 'a+')
+f.seek(0)
+data = f.read()
+f.close()
+# Verify previous data
+m = re.search(r'(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) -?\d+\.\d{2} (\d+\.\d{2}) \d+\.\d{4} (\d+\.\d{3}) \d+\.\d{2}', data)
+try:
+    tm = time.strptime(m.group(1), r'%Y/%m/%d %H:%M:%S')
+    cost = float(m.group(2))
+    energy = float(m.group(3))
+    log('Get previous data success.')
+except:
+    log('Get previous data failed.')
+# Main loop
+while True:
+    try:
+        if not cookie:
+            cookie = login(meter_user, meter_pass)
+        while cookie:
+            try:
+                balance = get_balance(cookie)
+                topup = get_topup(cookie)
+                energy_real = get_energy(cookie)
+                price, power = metering(balance, topup, energy_real)
+                # Output data
+                tm = time.localtime()
+                t = time.strftime(r'%Y/%m/%d %H:%M:%S', tm)
+                f = open(data_path, 'w')
+                f.write(f'{t} {balance:.2f} {cost:.2f} {price:.4f} {energy:.3f} {power:.2f}')
+                f.close()
+                print(f'{t}: {balance:7.2f} CNY, {cost:8.2f} CNY, {price:6.4f} CNY/kWh, {energy:9.3f} kWh, {energy_real:8.2f} kWh, {power:5.2f} kW')
+                balance_notify(balance)
+            except:
+                cookie = {}
+                break
+            else:
+                while time.localtime().tm_min == tm.tm_min or (time.localtime().tm_min - clock_offset) % data_intv:
+                    time.sleep(1)
+    finally:
+        time.sleep(10)
