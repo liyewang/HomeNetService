479d746a239c6f28a8f4a0ab7bb7f4ed6f7319b3
Liye Wang <liyewang0602@qq.com>
2022-03-16 16:30:15 +0800

    Add YAML config import feature. Minor improvements.

--- EnergyMeter.py
+++ EnergyMeter.py
@@ -12,14 +12,14 @@ from email.mime.text import MIMEText
 from email.header import Header
 import threading
 import socket
+import yaml
 
 # Energy Meter Config
 meter_user = 'user'
 meter_pass = 'pass'
-proxy = {}
-balance_low = -80
 #              Jan    Feb     Mar    Apr    May    Jun    Jul    Aug    Sep    Oct    Nov    Dec
 price_tab = [0.805, 0.711, 0.8251, 0.711, 0.711, 0.711, 0.738, 0.738, 0.738, 0.711, 0.711, 0.711]
+balance_low = -80
 data_intv = 30
 clock_offset = 1
 power_max = 15
@@ -27,13 +27,15 @@ energy_err_max = 0.01
 
 # Communication Config
 host = 'localhost'
-port = 23456
+port = 12345
 max_connect = 1
+proxy = None
 
 # File Path
 energy_meter_path = os.path.dirname(os.path.abspath(__file__))
 data_path = f'{energy_meter_path}/EnergyMeter'
 log_path = f'{energy_meter_path}/EnergyMeter.log'
+cfg_file = f'{energy_meter_path}/EnergyMeterCfg.yaml'
 
 # Mail Server
 mail_host = 'smtp.qq.com'
@@ -51,9 +53,12 @@ mail_text = 'Error message.'
 def log(msg):
     print(msg)
     t = time.strftime(r'%Y/%m/%d %H:%M:%S', time.localtime())
-    f = open(log_path, 'a')
-    f.write(f'{t}: {msg}\n')
-    f.close()
+    try:
+        with open(log_path, 'a') as f:
+            f.write(f'{t}: {msg}\n')
+    except:
+        pass
+    return
 
 def login(meter_user, meter_pass):
     url_login = 'http://tzjz.acrel-eem.com/Ajax/UserLogin.ashx?Id=' + str(random.random()) + '&username=' + meter_user + '&password=' + meter_pass
@@ -130,15 +135,21 @@ def balance_notify(balance):
                 message['From'] = Header(mail_from, 'utf-8')
                 message['To'] =  Header('<' + mail_to_addr + '>', 'utf-8')
                 message['Subject'] = Header(mail_subject, 'utf-8')
-                smtpObject.sendmail(mail_from_addr, [mail_to_addr], message.as_string())
-                log(f'Mail to [{mail_to_addr}] sent.')
+                try:
+                    smtpObject.sendmail(mail_from_addr, [mail_to_addr], message.as_string())
+                except:
+                    log(f'Mail to [{mail_to_addr}] blocked.')
+                else:
+                    log(f'Mail to [{mail_to_addr}] sent.')
         except:
             log('Sending mail failed.')
         else:
-            log('Sending mail success.')
             mail_balance -= 10
         finally:
-            smtpObject.quit()
+            try:
+                smtpObject.quit()
+            except:
+                pass
     elif balance > balance_low:
         mail_balance = balance_low
 
@@ -165,88 +176,120 @@ def comm_read():
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, port))
             data = s.recv(256).decode()
+            s.close()
             m = re.search(r'(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) (-?\d+) -?\d+\.\d{2} (\d+\.\d{2}) \d+\.\d{4} (\d+\.\d{3}) \d+\.\d{2}', data)
             print(m.group(0))
             attempt = 0
         except:
+            try:
+                s.close()
+            except:
+                pass
             log(f'Read data failed ({attempt}).')
             attempt -= 1
             time.sleep(1)
-        finally:
-            s.close()
 
-# Task selection
-if len(sys.argv) == 2:
-    if sys.argv[1] == 'read':
-        comm_read()
-    exit()
-elif len(sys.argv) > 2:
-    exit()
-# Init merter
-tm = time.localtime()
-energy_prev = 0
-cost_prev = 0
-energy = energy_prev
-cost = cost_prev
-mail_balance = balance_low
-cookie = {}
-msg = ''
-data_intv = round(data_intv)
-if data_intv <= 0:
-    data_intv = 1
-# Get previous data
-f = open(data_path, 'a+')
-f.seek(0)
-data = f.read()
-f.close()
-# Verify previous data
-m = re.search(r'(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) (-?\d+) -?\d+\.\d{2} (\d+\.\d{2}) \d+\.\d{4} (\d+\.\d{3}) \d+\.\d{2}', data)
-try:
-    tm = time.strptime(m.group(1), r'%Y/%m/%d %H:%M:%S')
-    mail_balance = int(m.group(2))
-    cost = float(m.group(3))
-    energy = float(m.group(4))
-    log('Get previous data success.')
-except:
-    log('Get previous data failed.')
-# Start tasks
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-    s.listen(max_connect)
-    threading.Thread(target=comm_task).start()
-except:
-    log('Task start failed.')
-else:
-    log('Task start success.')
-    # Main loop
-    while True:
-        if not cookie:
-            try:
-                cookie = login(meter_user, meter_pass)
-            except:
-                cookie = {}
-        while cookie:
-            try:
-                balance = get_balance(cookie)
-                topup = get_topup(cookie)
-                energy_real = get_energy(cookie)
-            except:
-                cookie = {}
-                break
-            else:
-                price, power = metering(balance, topup, energy_real)
-                balance_notify(balance)
-                tm = time.localtime()
-                t = time.strftime(r'%Y/%m/%d %H:%M:%S', tm)
-                msg = f'{t} {mail_balance} {balance:.2f} {cost:.2f} {price:.4f} {energy:.3f} {power:.2f}'
+if __name__ == '__main__':
+    # Load config
+    with open(cfg_file, 'r') as f:
+        cfg = yaml.safe_load(f)
+    # Energy Meter Config
+    meter_user = cfg.get('meter_user', meter_user)
+    meter_pass = cfg.get('meter_pass', meter_pass)
+    price_tab = cfg.get('price_tab', price_tab)
+    balance_low = cfg.get('balance_low', balance_low)
+    data_intv = cfg.get('data_intv', data_intv)
+    clock_offset = cfg.get('clock_offset', clock_offset)
+    power_max = cfg.get('power_max', power_max)
+    energy_err_max = cfg.get('energy_err_max', energy_err_max)
+    # Communication Config
+    host = cfg.get('host', host)
+    port = cfg.get('port', port)
+    max_connect = cfg.get('max_connect', max_connect)
+    proxy = cfg.get('proxy', proxy)
+    # Mail Server
+    mail_host = cfg.get('mail_host', mail_host)
+    mail_port = cfg.get('mail_port', mail_port)
+    mail_user = cfg.get('mail_user', mail_user)
+    mail_pass = cfg.get('mail_pass', mail_pass)
+    # Mail Address
+    mail_from_addr = cfg.get('mail_from_addr', mail_from_addr)
+    mail_to_addrs = cfg.get('mail_to_addrs', mail_to_addrs)
+
+    # Task selection
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'read':
+            comm_read()
+        exit()
+    elif len(sys.argv) > 2:
+        exit()
+    # Init merter
+    tm = time.localtime()
+    energy_prev = 0
+    cost_prev = 0
+    energy = energy_prev
+    cost = cost_prev
+    mail_balance = balance_low
+    cookie = {}
+    msg = ''
+    data_intv = round(data_intv)
+    if data_intv <= 0:
+        data_intv = 1
+    # Get previous data
+    try:
+        with open(data_path, 'a+') as f:
+            f.seek(0)
+            data = f.read()
+            # Verify previous data
+            m = re.search(r'(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}) (-?\d+) -?\d+\.\d{2} (\d+\.\d{2}) \d+\.\d{4} (\d+\.\d{3}) \d+\.\d{2}', data)
+            tm = time.strptime(m.group(1), r'%Y/%m/%d %H:%M:%S')
+            mail_balance = int(m.group(2))
+            cost = float(m.group(3))
+            energy = float(m.group(4))
+    except:
+        log('Get previous data failed.')
+    else:
+        log('Get previous data success.')
+    # Start tasks
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host, port))
+        s.listen(max_connect)
+        threading.Thread(target=comm_task).start()
+    except:
+        log('Task start failed.')
+    else:
+        log('Task start success.')
+        # Main loop
+        while True:
+            if not cookie:
                 try:
-                    f = open(data_path, 'w')
-                    f.write(msg)
-                    f.close()
+                    cookie = login(meter_user, meter_pass)
                 except:
-                    pass
-                print(f'{t}: {mail_balance:4d} CNY {balance:7.2f} CNY, {cost:8.2f} CNY, {price:6.4f} CNY/kWh, {energy:9.3f} kWh, {energy_real:8.2f} kWh, {power:5.2f} kW')
-                while time.localtime().tm_min == tm.tm_min or (time.localtime().tm_min - clock_offset) % data_intv:
-                    time.sleep(1)
-        time.sleep(10)
+                    cookie = {}
+                    log('Login failed.')
+                else:
+                    log('Login success.')
+            while cookie:
+                try:
+                    balance = get_balance(cookie)
+                    topup = get_topup(cookie)
+                    energy_real = get_energy(cookie)
+                except:
+                    cookie = {}
+                    break
+                else:
+                    price, power = metering(balance, topup, energy_real)
+                    balance_notify(balance)
+                    tm = time.localtime()
+                    t = time.strftime(r'%Y/%m/%d %H:%M:%S', tm)
+                    msg = f'{t} {mail_balance} {balance:.2f} {cost:.2f} {price:.4f} {energy:.3f} {power:.2f}'
+                    try:
+                        with open(data_path, 'w') as f:
+                            f.write(msg)
+                    except:
+                        pass
+                    print(f'{t}: {mail_balance:4d} CNY {balance:7.2f} CNY, {cost:8.2f} CNY, {price:6.4f} CNY/kWh, {energy:9.3f} kWh, {energy_real:8.2f} kWh, {power:5.2f} kW')
+                    while time.localtime().tm_min == tm.tm_min or (time.localtime().tm_min - clock_offset) % data_intv:
+                        time.sleep(1)
+            time.sleep(10)
--- ip_monitor.py
+++ ip_monitor.py
@@ -9,11 +9,13 @@ import time
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+import yaml
 
 # File Path
 ip_monitor_path = os.path.dirname(os.path.abspath(__file__))
 ip_store_path = f'{ip_monitor_path}/ip_address'
-log_path = f'{ip_monitor_path}/monitor.log'
+log_path = f'{ip_monitor_path}/ip_monitor.log'
+cfg_file = f'{ip_monitor_path}/ip_monitor_cfg.yaml'
 cert_path = '/root/IPsec_Certs'
 cert_info_path = f'{cert_path}/cert_info'
 ipsec_conf_path = '/etc/ipsec.conf'
@@ -34,17 +36,18 @@ mail_text = 'Error message.'
 def log(msg):
     print(msg)
     t = time.strftime(r'%Y/%m/%d %H:%M:%S', time.localtime())
-    fileObject = open(log_path, 'a')
-    fileObject.write(f'{t}: {msg}\n')
-    fileObject.close()
+    try:
+        with open(log_path, 'a') as fileObject:
+            fileObject.write(f'{t}: {msg}\n')
+    except:
+        pass
     return
 
 def ipsec_update():
     # Update cert_info
     try:
-        fileObject = open(cert_info_path, 'r')
-        fileStr = fileObject.read()
-        fileObject.close()
+        with open(cert_info_path, 'r') as fileObject:
+            fileStr = fileObject.read()
         searchObject = re.search(r'ADDR=(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', fileStr)
         if searchObject:
             cert_addr_prev = searchObject.group(1)
@@ -52,9 +55,8 @@ def ipsec_update():
             log('Get previous cert address failed.')
             raise RuntimeError('Key process failed.')
         fileStrUpdate = re.sub(cert_addr_prev, ip, fileStr)
-        fileObject = open(cert_info_path, 'w')
-        fileObject.write(fileStrUpdate)
-        fileObject.close
+        with open(cert_info_path, 'w') as fileObject:
+            fileObject.write(fileStrUpdate)
     except:
         log('Update cert_info failed.')
         raise RuntimeError('Key process failed.')
@@ -98,13 +100,11 @@ def ipsec_update():
 
     # Update ipsec.conf
     try:
-        fileObject = open(ipsec_conf_path, 'r')
-        fileStr = fileObject.read()
-        fileObject.close()
+        with open(ipsec_conf_path, 'r') as fileObject:
+            fileStr = fileObject.read()
         fileStrUpdate = re.sub(cert_addr_prev, ip, fileStr)
-        fileObject = open(ipsec_conf_path, 'w')
-        fileObject.write(fileStrUpdate)
-        fileObject.close
+        with open(ipsec_conf_path, 'w') as fileObject:
+            fileObject.write(fileStrUpdate)
     except:
         log('Update ipsec.conf failed.')
         raise RuntimeError('Key process failed.')
@@ -117,64 +117,82 @@ def ipsec_update():
     os.system('systemctl restart ipsec')
     return
 
-# Get current IP address
-try:
-    socketObject = socket.create_connection(('ns1.dnspod.net', 6666))
-    ip = socketObject.recv(16).decode('utf-8')
-    socketObject.close()
-except:
+if __name__ == '__main__':
+    # Load config
+    with open(cfg_file, 'r') as f:
+        cfg = yaml.safe_load(f)
+    # File Path
+    cert_path = cfg.get('cert_path', cert_path)
+    ipsec_conf_path = cfg.get('ipsec_conf_path', ipsec_conf_path)
+    # Mail Server
+    mail_host = cfg.get('mail_host', mail_host)
+    mail_port = cfg.get('mail_port', mail_port)
+    mail_user = cfg.get('mail_user', mail_user)
+    mail_pass = cfg.get('mail_pass', mail_pass)
+    # Mail Address
+    mail_from_addr = cfg.get('mail_from_addr', mail_from_addr)
+    mail_to_addrs = cfg.get('mail_to_addrs', mail_to_addrs)
+
+    # Get current IP address
     try:
-        ip = requests.get('http://checkip.dyndns.com', timeout=5).text.strip()
+        socketObject = socket.create_connection(('ns1.dnspod.net', 6666))
+        ip = socketObject.recv(16).decode('utf-8')
+        socketObject.close()
     except:
         try:
-            ip = requests.get('http://ifconfig.me/ip', timeout=5).text.strip()
+            socketObject.close()
         except:
-            ip = ''
-
-# Verify current IP address
-searchObject = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip)
-if searchObject:
-    ip = searchObject.group()
-
-    # Get previous IP address
-    fileObject = open(ip_store_path, 'a+')
-    fileObject.seek(0)
-    ip_prev = fileObject.read()
-    fileObject.close()
-    # Verify previous IP address
-    searchObject = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_prev)
-    if searchObject:
-        ip_prev = searchObject.group()
-    else:
-        log('Get previous IP address failed.')
-
-    # Check IP address change
-    if ip != ip_prev:
-        log(f'IP address changed from [{ip_prev}] to [{ip}]')
-        fileObject = open(ip_store_path, 'w')
-        fileObject.write(ip)
-        fileObject.close()
-        ipsec_update()
-
-        # Mail IP address change message
-        mail_text = f'New VPN Server IP address: {ip}'
+            pass
         try:
-            smtpObject = smtplib.SMTP_SSL(mail_host, mail_port)
-            smtpObject.login(mail_user, mail_pass)
-            for mail_to_addr in mail_to_addrs:
-                message = MIMEText(mail_text, 'plain', 'utf-8')
-                message['From'] = Header(mail_from, 'utf-8')
-                message['To'] =  Header('<' + mail_to_addr + '>', 'utf-8')
-                message['Subject'] = Header(mail_subject, 'utf-8')
-                smtpObject.sendmail(mail_from_addr, [mail_to_addr], message.as_string())
-                log(f'Mail to [{mail_to_addr}] sent.')
+            ip = requests.get('http://checkip.dyndns.com', timeout=5).text.strip()
         except:
-            log('Sending mail failed.')
+            try:
+                ip = requests.get('http://ifconfig.me/ip', timeout=5).text.strip()
+            except:
+                ip = ''
+
+    # Verify current IP address
+    searchObject = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip)
+    if searchObject:
+        ip = searchObject.group()
+
+        # Get previous IP address
+        with open(ip_store_path, 'a+') as fileObject:
+            fileObject.seek(0)
+            ip_prev = fileObject.read()
+        # Verify previous IP address
+        searchObject = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip_prev)
+        if searchObject:
+            ip_prev = searchObject.group()
         else:
-            log('Sending mail success.')
-        finally:
-            smtpObject.quit()
+            log('Get previous IP address failed.')
+
+        # Check IP address change
+        if ip != ip_prev:
+            log(f'IP address changed from [{ip_prev}] to [{ip}]')
+            with open(ip_store_path, 'w') as fileObject:
+                fileObject.write(ip)
+            ipsec_update()
+
+            # Mail IP address change message
+            mail_text = f'New VPN Server IP address: {ip}'
+            try:
+                smtpObject = smtplib.SMTP_SSL(mail_host, mail_port)
+                smtpObject.login(mail_user, mail_pass)
+                for mail_to_addr in mail_to_addrs:
+                    message = MIMEText(mail_text, 'plain', 'utf-8')
+                    message['From'] = Header(mail_from, 'utf-8')
+                    message['To'] =  Header('<' + mail_to_addr + '>', 'utf-8')
+                    message['Subject'] = Header(mail_subject, 'utf-8')
+                    smtpObject.sendmail(mail_from_addr, [mail_to_addr], message.as_string())
+                    log(f'Mail to [{mail_to_addr}] sent.')
+            except:
+                log('Sending mail failed.')
+            else:
+                log('Sending mail success.')
+            finally:
+                smtpObject.quit()
+        else:
+            print(f'IP address [{ip}] unchanged.')
     else:
-        print(f'IP address [{ip}] unchanged.')
-else:
-    log('Get IP address failed.')
+        log('Get IP address failed.')
