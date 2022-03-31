70f5138664078ea0062bf81908cfe572e74dec64
Liye Wang <liyewang0602@qq.com>
2022-03-31 19:02:50 +0800

    Add retry feature to proxy test script.

--- tools/proxy_test.sh
+++ tools/proxy_test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 URL="google.com"
 PROXY="socks5://127.0.0.1:1080"
-RESULT=`curl -so /dev/null -w "%{http_code}" $URL -x $PROXY`
-if [ "$RESULT" == "200" ] || [ "$RESULT" == "301" ]; then
-    echo "Access $URL: Success [$RESULT]"
-else
-    echo "Access $URL: Failed [$RESULT]"
-fi
+RETRY=3
+while [ $RETRY -gt 0 ]
+do
+    RESULT=`curl -so /dev/null -w "%{http_code}" $URL -x $PROXY`
+    if [ "$RESULT" == "200" ] || [ "$RESULT" == "301" ]; then
+        echo "Access $URL: Success [$RESULT]"
+        break
+    else
+        echo "Access $URL: Failed [$RESULT]"
+    fi
+    RETRY=`expr $RETRY - 1`
+    sleep 3s
+done
